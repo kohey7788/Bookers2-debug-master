de04db86ff07095826d945619c54bab64e9f885b
EC2 Default User <ec2-user@ip-172-31-10-190.ec2.internal>
2021-02-08 06:48:17 +0000

    4th Commit

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   end
   get 'users/:id/relationships/show_follow' => 'relationships#show_follow', as: 'follow'
   get 'users/:id/relationships/show_follower' => 'relationships#show_follower', as: 'follower'
+  get '/search' => 'search#search'
 end
